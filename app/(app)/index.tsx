dbc124c9d202b0997de46a9a17e55af757bd19b4
John Lee <leejohn10234@gmail.com>
2026-07-12 16:56:57 -0700

    Added supabase integration. Simple signIn/out working confirmed

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,5 +1,16 @@
-import { Text } from "react-native";
+import { router } from "expo-router";
+import { Button, Text, View } from "react-native";
 
 export default function HomeScreen() {
-  return <Text style={{ color: "red" }}>Home</Text>;
+  return (
+    <View>
+      <Text style={{ color: "red" }}>Home</Text>
+      <Button
+        title={"sign out"}
+        onPress={() => {
+          router.replace("/(auth)");
+        }}
+      />
+    </View>
+  );
 }
--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,13 +1,52 @@
+import { supabase } from "@/utils/supabase";
 import { router } from "expo-router";
-import { Button, View } from "react-native";
+import { useState } from "react";
+import { Alert, Button, TextInput, View } from "react-native";
 
 export default function LogInScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function handleUserSignIn() {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      console.log("error.message");
+      Alert.alert("Login failed", "Incorrect email or password.");
+      return;
+    }
+
+    console.log(data);
+    router.replace("/(app)");
+  }
+
   return (
     <View>
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+        autoCapitalize="none"
+        style={{ color: "white" }}
+      />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+        autoCapitalize="none"
+        style={{ color: "white" }}
+      />
+
       <Button
-        title={"Log In"}
+        title={"Sign In"}
         onPress={() => {
-          router.replace("/(app)");
+          handleUserSignIn();
         }}
       />
     </View>
